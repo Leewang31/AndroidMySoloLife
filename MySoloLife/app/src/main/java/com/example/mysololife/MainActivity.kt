f17224ffbd1d884e607ab5a7456d9cdb891204fc
LeeWang <kiwan1111@gmail.com>
2022-05-29 00:45:38 +0900

    Step.3

--- MySoloLife/app/src/main/java/com/example/mysololife/MainActivity.kt
+++ MySoloLife/app/src/main/java/com/example/mysololife/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.mysololife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.mysololife.auth.IntroActivity
+import com.example.mysololife.databinding.ActivityJoinBinding
+import com.example.mysololife.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.LogoutBtn.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(this,"로그아웃",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
--- MySoloLife/app/src/main/java/com/example/mysololife/auth/JoinActivity.kt
+++ MySoloLife/app/src/main/java/com/example/mysololife/auth/JoinActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
 import com.example.mysololife.databinding.ActivityIntroBinding
 import com.example.mysololife.databinding.ActivityJoinBinding
@@ -21,44 +23,57 @@ class JoinActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        var isGoToJoin = true
         binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
 
         binding.joinBtn.setOnClickListener {
+
+
+            auth = Firebase.auth
 
             val email = binding.emailArea.text.toString()
             val password1 = binding.passwordArea1.text.toString()
             val password2 = binding.passwordArea2.text.toString()
 
             // 저기 값이 비어있는 지 확인
-            if(email.isEmpty()){
-                Toast.makeText(this,"이메일을 입력해주세요.",Toast.LENGTH_LONG).show()
+            if (email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
             }
-            else if(password1.isEmpty()){
-                Toast.makeText(this,"비밀번호를 입력해주세요.",Toast.LENGTH_LONG).show()
+            if (password1.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
             }
-            else if(password2.isEmpty()){
-                Toast.makeText(this,"비밀번호 확인을 입력해주세요.",Toast.LENGTH_LONG).show()
+            if (password2.isEmpty()) {
+                Toast.makeText(this, "비밀번호 확인을 입력해주세요.", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
             }
-
+            //6자 이상인지
+             if (password1.length < 6) {
+                Toast.makeText(this, "비밀번호를 6자 이상 해주세요.", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
             //비밀번호 확인인
-           if(password1.equals(password2)){
-
-            }else{
-                Toast.makeText(this,"비밀번호가 다릅니다.",Toast.LENGTH_LONG).show()
+             if (!password1.equals(password2)) {
+                Toast.makeText(this, "비밀번호가 다릅니다.", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+            if (isGoToJoin) {
+                auth.createUserWithEmailAndPassword(email, password1)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "회원가입 성공", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "회원가입 실패", Toast.LENGTH_LONG).show()
+                        }
+                    }
             }
         }
 
-        auth = Firebase.auth
 
-
-        /*auth.createUserWithEmailAndPassword("abc@abc.abc", "abcdabcd")
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this,"성공",Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(this,"실패",Toast.LENGTH_LONG).show()
-                }
-            }*/
     }
 
 }
