79376f2ee5667ca1b6108d8e52652cbeef7c7c25
Leewang31 <kiwan1111@gmail.com>
2022-06-05 20:57:41 +0900

    Step.5

--- MySoloLife/app/src/main/java/com/example/mysololife/cotentsList/ContentsRVAdapter.kt
+++ MySoloLife/app/src/main/java/com/example/mysololife/cotentsList/ContentsRVAdapter.kt
@@ -1,14 +1,25 @@
 package com.example.mysololife.cotentsList
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mysololife.R
 
 //아이템을 가져오고
-class ContentsRVAdapter(val items: ArrayList<String>) :
+class ContentsRVAdapter(val context : Context, val items: ArrayList<ContentModel>) :
     RecyclerView.Adapter<ContentsRVAdapter.Viewholder>() {
+
+    interface ItemClick{
+        fun onClick(view : View , position :Int)
+    }
+    var itemClick : ItemClick? = null
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -20,6 +31,12 @@ class ContentsRVAdapter(val items: ArrayList<String>) :
 
     //아이템을 넣을수 있게함
     override fun onBindViewHolder(holder: ContentsRVAdapter.Viewholder, position: Int) {
+
+        if(itemClick != null){
+            holder.itemView.setOnClickListener{ v->
+                itemClick?.onClick(v,position)
+            }
+        }
         holder.bindItems(items[position])
     }
 
@@ -28,12 +45,20 @@ class ContentsRVAdapter(val items: ArrayList<String>) :
         return items.size
     }
 
+
+
     inner class Viewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bindItems(item: String) {
+        fun bindItems(item: ContentModel) {
+
+            val contentTitle = itemView.findViewById<TextView>(R.id.textArea)
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+            contentTitle.text = item.title
+
+            Glide.with(context)
+                .load(item.imageUrl)
+                .into(imageViewArea)
 
         }
-
     }
-
 }
